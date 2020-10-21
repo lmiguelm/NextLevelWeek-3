4c2efc4b6d4766bc1fa2731cf49b2978d1cac1e2
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-10-21 18:34:32 -0300

    adicionando coluna 'pending' na tabela de orphanages

---
+++ backend/src/database/migrations/1603315881034-add_column_pending_on_orphanages.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class changeOrphanages1603315453276 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('orphanages', new TableColumn({
+            name: 'pending',
+            type: 'bolean',
+            default: false
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('orphanages', 'pending');
+    }
+
+}
